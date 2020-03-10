88030d22a71588cee501d46f880a98f030fb0d8c
Daniel Bittencourt <dbittencourt@ermpower.com.au>
2020-03-10 22:29:32 +1100

    added cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { SERVER_PORT } from './config';
 
 const server = express();
+
+server.use(cors());
 server.use(bodyParser.json());
 
 server.listen(SERVER_PORT, () => {
