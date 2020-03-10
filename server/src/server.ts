3e01f457e1022df629abce452a7d51c4b548fe77
Daniel Bittencourt <dbittencourt@ermpower.com.au>
2020-03-10 23:58:19 +1100

    added helmet

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import helmet from 'helmet';
 import { SERVER_PORT } from './config';
 
 const server = express();
 
 //todo: add auth
+server.use(helmet());
 server.use(cors());
 server.use(bodyParser.json());
 
